13df2898f2576aef49a07910414294f2a737fd3a
Japaneixxx <91101276+Japaneixxx@users.noreply.github.com>
2025-06-11 15:06:10 +0000

    Adiciona estrutura inicial do projeto com Maven e Spring Boot

---
+++ src/test/java/com/japaneixxx/pagueleve/PagueleveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.japaneixxx.pagueleve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagueleveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
